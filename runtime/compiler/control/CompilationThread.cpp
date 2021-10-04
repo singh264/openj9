dd0b9f3b5c5b8e4c7afd86330b8317134f17bc04
Amarpreet Singh <amarpreet1997@gmail.com>
2021-10-04 16:03:38 -0400

    Rename the methods
    I think the methods are renamed based on the
    following code review comments:
    https://github.com/eclipse-openj9/openj9/pull/13413#discussion_r711273364.
    https://github.com/eclipse-openj9/openj9/pull/13413#discussion_r711272325.
    https://github.com/eclipse-openj9/openj9/pull/13413#discussion_r711268116.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- runtime/compiler/control/CompilationThread.cpp
+++ runtime/compiler/control/CompilationThread.cpp
@@ -7919,14 +7919,14 @@ TR::CompilationInfoPerThreadBase::compile(J9VMThread * vmThread,
    }
 
 bool
-TR::CompilationInfoPerThreadBase::isCompilingWithExcessiveCacheSize(TR::CompilationInfoPerThreadBase *compilationInfo, CompileParameters *compileParameters)
+TR::CompilationInfoPerThreadBase::isCodeOrDataCacheFull(TR::CompilationInfoPerThreadBase *compilationInfo, CompileParameters *compileParameters)
    {
       J9JITConfig *jitConfig = compilationInfo->_jitConfig;
       J9VMThread *vmThread = compileParameters->_vmThread;
 
       if (!(jitConfig->runtimeFlags & (J9JIT_CODE_CACHE_FULL | J9JIT_DATA_CACHE_FULL)))
          {
-            return false;
+         return false;
          }
 
       // Optimization to disable future first time compilations from reaching the queue
@@ -7946,15 +7946,16 @@ TR::CompilationInfoPerThreadBase::isCompilingWithExcessiveCacheSize(TR::Compilat
    }
 
 bool
-TR::CompilationInfoPerThreadBase::isCompilingRestrictedMethod(TR::CompilationInfoPerThreadBase *compilationInfo, TR_ResolvedMethod *compilee, CompileParameters *compileParameters, TR_FilterBST *&filterInfo)
+TR::CompilationInfoPerThreadBase::isRestrictedMethod(TR::CompilationInfoPerThreadBase *compilationInfo, TR_ResolvedMethod *compilee, CompileParameters *compileParameters, TR_FilterBST *&filterInfo)
    {
       TR_J9VMBase *vm = compileParameters->_vm;
       J9VMThread *vmThread = compileParameters->_vmThread;
 
       // JITServer: methodCanBeCompiled check should have been done on the client, skip it on the server.
-      if (compilationInfo->_methodBeingCompiled->isOutOfProcessCompReq() || compilationInfo->methodCanBeCompiled(compileParameters->trMemory(), vm, compilee, filterInfo))
+      if (compilationInfo->_methodBeingCompiled->isOutOfProcessCompReq() ||
+          compilationInfo->methodCanBeCompiled(compileParameters->trMemory(), vm, compilee, filterInfo))
          {
-            return false;
+         return false;
          }
 
       compilationInfo->_methodBeingCompiled->_compErrCode = compilationRestrictedMethod;
@@ -7972,7 +7973,7 @@ TR::CompilationInfoPerThreadBase::isCompilingRestrictedMethod(TR::CompilationInf
    }
 
 void
-TR::CompilationInfoPerThreadBase::setCompiledMethodOptimzationPlan(TR::CompilationInfoPerThreadBase *compilationInfo, TR_ResolvedMethod *compilee, TR_J9VMBase *vm)
+TR::CompilationInfoPerThreadBase::addUpgradeHintInSCCIfNeeded(TR::CompilationInfoPerThreadBase *compilationInfo, TR_ResolvedMethod *compilee, TR_J9VMBase *vm)
    {
       if (compilationInfo->_methodBeingCompiled->_optimizationPlan->isUpgradeRecompilation())
          {
@@ -8012,9 +8013,10 @@ TR::CompilationInfoPerThreadBase::createCompilee(TR::CompilationInfoPerThreadBas
          compilee = vm->createResolvedMethod(compileParameters->trMemory(), method);
          }
 
-      setCompiledMethodOptimzationPlan(compilationInfo, compilee, vm);
+      addUpgradeHintInSCCIfNeeded(compilationInfo, compilee, vm);
 
-      if (isCompilingRestrictedMethod(compilationInfo, compilee, compileParameters, filterInfo) || isCompilingWithExcessiveCacheSize(compilationInfo, compileParameters))
+      if (isRestrictedMethod(compilationInfo, compilee, compileParameters, filterInfo) ||
+          isCodeOrDataCacheFull(compilationInfo, compileParameters))
          {
          compilee = 0;
          }
