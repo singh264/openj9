57c15679275e94cdb27c638181b38e3b1185212c
Amarpreet Singh <amarpreet1997@gmail.com>
2021-11-02 13:53:06 -0400

    buildVirtualDispatch

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -1913,43 +1913,34 @@ J9::Z::PrivateLinkage::buildVirtualDispatch(TR::Node * callNode, TR::RegisterDep
       // load class pointer
       TR::Register *classReg = vftReg;
 
-      if (!TR::Compiler->cls.classesOnHeap() && offset >= 0 && methodSymRef == comp()->getSymRefTab()->findObjectNewInstanceImplSymbol())
+      // It should be impossible to have a offset that can't fit in 20bit given Java method table limitations.
+      // We assert here to insure limitation/assumption remains true. If this fires we need to fix this code
+      // and the _virtualUnresolvedHelper() code to deal with a new worst case scenario for patching.
+      TR_ASSERT_FATAL(offset>MINLONGDISP, "JIT VFT offset does not fit in 20bits");
+      TR_ASSERT_FATAL(offset!=0 || unresolvedSnippet, "Offset is 0 yet unresolvedSnippet is NULL");
+      TR_ASSERT_FATAL(offset<=MAX_IMMEDIATE_VAL, "Offset is larger then MAX_IMMEDIATE_VAL");
+
+      // If unresolved/AOT, this instruction will be patched by _virtualUnresolvedHelper() with the correct offset
+      cursor = generateRXInstruction(cg(), TR::InstOpCode::getExtendedLoadOpCode(), callNode, RegRA,
+                                       generateS390MemoryReference(classReg, offset, cg()));
+
+      if (unresolvedSnippet)
          {
-         cursor =
-            generateRXInstruction(cg(), TR::InstOpCode::getLoadOpCode(), callNode, RegRA, generateS390MemoryReference(classReg, offset, cg()));
+         ((TR::S390VirtualUnresolvedSnippet *)unresolvedSnippet)->setPatchVftInstruction(cursor);
+         }
+
+      // A load immediate into R0 instruction (LHI/LGFI) MUST be generated here because the "LA" instruction used by
+      // the VM to find VFT table entries can't handle negative displacements. For unresolved/AOT targets we must assume
+      // the worse case (offset can't fit in 16bits). VFT offset 0 means unresolved/AOT, otherwise offset is negative.
+      // Some special cases have positive offsets i.e. java/lang/Object.newInstancePrototype()
+      if (!unresolvedSnippet && offset >= MIN_IMMEDIATE_VAL && offset <= MAX_IMMEDIATE_VAL) // Offset fits in 16bits
+         {
          cursor = generateRIInstruction(cg(), TR::InstOpCode::getLoadHalfWordImmOpCode(), callNode, RegZero, offset);
          }
-      else
+      else // if unresolved || offset can't fit in 16bits
          {
-         // It should be impossible to have a offset that can't fit in 20bit given Java method table limitations.
-         // We assert here to insure limitation/assumption remains true. If this fires we need to fix this code
-         // and the _virtualUnresolvedHelper() code to deal with a new worst case scenario for patching.
-         TR_ASSERT_FATAL(offset>MINLONGDISP, "JIT VFT offset does not fit in 20bits");
-         TR_ASSERT_FATAL(offset!=0 || unresolvedSnippet, "Offset is 0 yet unresolvedSnippet is NULL");
-         TR_ASSERT_FATAL(offset<=MAX_IMMEDIATE_VAL, "Offset is larger then MAX_IMMEDIATE_VAL");
-
          // If unresolved/AOT, this instruction will be patched by _virtualUnresolvedHelper() with the correct offset
-         cursor = generateRXInstruction(cg(), TR::InstOpCode::getExtendedLoadOpCode(), callNode, RegRA,
-                                        generateS390MemoryReference(classReg, offset, cg()));
-
-         if (unresolvedSnippet)
-            {
-            ((TR::S390VirtualUnresolvedSnippet *)unresolvedSnippet)->setPatchVftInstruction(cursor);
-            }
-
-         // A load immediate into R0 instruction (LHI/LGFI) MUST be generated here because the "LA" instruction used by
-         // the VM to find VFT table entries can't handle negative displacements. For unresolved/AOT targets we must assume
-         // the worse case (offset can't fit in 16bits). VFT offset 0 means unresolved/AOT, otherwise offset is negative.
-         // Some special cases have positive offsets i.e. java/lang/Object.newInstancePrototype()
-         if (!unresolvedSnippet && offset >= MIN_IMMEDIATE_VAL && offset <= MAX_IMMEDIATE_VAL) // Offset fits in 16bits
-            {
-            cursor = generateRIInstruction(cg(), TR::InstOpCode::getLoadHalfWordImmOpCode(), callNode, RegZero, offset);
-            }
-         else // if unresolved || offset can't fit in 16bits
-            {
-            // If unresolved/AOT, this instruction will be patched by _virtualUnresolvedHelper() with the correct offset
-            cursor = generateRILInstruction(cg(), TR::InstOpCode::LGFI, callNode, RegZero, static_cast<int32_t>(offset));
-            }
+         cursor = generateRILInstruction(cg(), TR::InstOpCode::LGFI, callNode, RegZero, static_cast<int32_t>(offset));
          }
 
       gcPoint = new (trHeapMemory()) TR::S390RRInstruction(TR::InstOpCode::BASR, callNode, RegRA, RegRA, cg());
